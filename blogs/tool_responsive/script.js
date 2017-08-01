4d6892f3bb9fbf1a95749877dab77de911702e8c
兔子哥 <m13721061552@163.com>
2017-08-01 11:59:05 +0800

    Site updated: 2017-08-01 11:59:04

--- blogs/tool_responsive/script.js
+++ blogs/tool_responsive/script.js
@@ -19,9 +19,9 @@ bm_ns.add_row = function(e) {
 		'<div class="row">' +
 		'<ul>' +
 			'<li class="col0 sort">&nbsp;</li>' +
-			'<li class="col1"><input type="text" value=""></li>' +
-			'<li class="col2"><input type="text" value=""></li>' +
-			'<li class="col3"><input type="text" value=""></li>' +
+			'<li class="col1"><input type="text" value="" /></li>' +
+			'<li class="col2"><input type="text" value="" /></li>' +
+			'<li class="col3"><input type="text" value="" /></li>' +
 			'<li class="col4 colN del">x</li>' +
 		'</ul>' +
 		'<div class="clear"></div>' +
@@ -33,7 +33,7 @@ bm_ns.delete_row = function(e) {
 }
 
 bm_ns.generate_bookmarklet = function(highlight) {
-	var link = "javascript:document.write('<!DOCTYPE html><html><head>&lt;meta charset=&quot;utf-8&quot;><title>Responsive Design Testing</title><style>body { margin: 20px; font-family: sans-serif; overflow-x: scroll; }.wrapper { width: 6000px; }.frame { float: left; }h2 { margin: 0 0 5px 0; }iframe { margin: 0 20px 20px 0; border: 1px solid #666; }</style></head><body><div class="&quot;wrapper&quot;">";
+	var link = "javascript:document.write('<!DOCTYPE html><html><head>&lt;meta charset=&quot;utf-8&quot;><title>Responsive Design Testing</title><style>body { margin: 20px; font-family: sans-serif; overflow-x: scroll; }.wrapper { width: 6000px; }.frame { float: left; }h2 { margin: 0 0 5px 0; }iframe { margin: 0 20px 20px 0; border: 1px solid #666; }</style></head><body><div class=&quot;wrapper&quot;>";
 	$("#bm_generate .sortable .row").each(function() {
 		var width  = $(this).find(".col1 input").val();
 		var height = $(this).find(".col2 input").val();
@@ -41,8 +41,9 @@ bm_ns.generate_bookmarklet = function(highlight) {
 		if (label) {
 			label = "(" + label + ")";
 		}
-		link += "<div class="&quot;frame&quot;"><h2>" + width + "<span> x " + height + "</span> <small>" + label + "</small></h2>"
-					+ "<iframe src="&quot;'" +="" window.location="" '&quot;="" sandbox="&quot;allow-same-origin" allow-scripts="" allow-forms&quot;="" seamless "="" "width="&quot;"" width="" "&quot;="" height="&quot;""></iframe></div>"
+		link += "<div class=&quot;frame&quot;><h2>" + width + "<span> x " + height + "</span> <small>" + label + "</small></h2>"
+					+ "<iframe src=&quot;' + window.location + '&quot; sandbox=&quot;allow-same-origin allow-scripts allow-forms&quot; seamless "
+					+ "width=&quot;" + width + "&quot; height=&quot;" + height + "&quot;></iframe></div>"
 	});
 	link += "</div></body></html>')";
 	$("#bookmarklet_link").html('<a href="' + link + '">Responsive Design Test</a>');
